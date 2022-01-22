cb9edf7fe4b140d0ef5613ddf0e0a1dfc6dbb48a
sanghyeon <tjtkdgus45@nate.com>
2022-01-22 13:54:27 +0900

    22.01.21 connect to frontend

--- config/settings.py
+++ config/settings.py
@@ -80,7 +80,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -159,4 +159,5 @@ AUTH_USER_MODEL = "mainApp.User"  # <app명, clss명>
 
 # MEDIA_URL = "/media/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
