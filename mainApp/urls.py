7917acf44aab2db054a7e6a87c2590ebdac00ad6
Seo-sang <tjtkdgus45@nate.com>
2022-02-09 16:25:33 +0900

    upload modified files

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -5,9 +5,9 @@ app_name = "mainApp"
 
 urlpatterns = [
     path("", views.mainView.as_view(),name='mainpage'),
+    path("/<int:pk>/", views.TeacherView.as_view()),
     path("login/",views.login_view,name='login'),
     path("signup/", views.signup, name='signup'),
     path("logout/",views.logout,name='logout'),
     path("lan/", views.TeachableUserView.as_view()),
-    path("lan/<int:pk>/", views.TeacherView.as_view()),
 ]
